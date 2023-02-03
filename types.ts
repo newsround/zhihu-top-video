2849a1cd89a0929f5bb8ce52a55264cf467845c3
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-02-03 22:31:12 +0900

    fix types

--- types.ts
+++ types.ts
@@ -1,8 +1,17 @@
+export type Item = {
+  target: {
+    title_area: {
+      text: string;
+    };
+    link: {
+      url: string;
+    };
+  };
+};
 export type Question = {
   title: string;
   url: string;
 };
-
 export type HotList = {
   data: Item[];
 };
