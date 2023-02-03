a5f51a68dea22eb42d2674fd17eb1f7eccaabe7b
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-02-03 22:24:36 +0900

    fix util

--- utils.ts
+++ utils.ts
@@ -24,7 +24,7 @@ export function createList(words: Question[]): string {
   return `<!-- BEGIN -->
 <!-- UpdateTime ${Date()} -->
 ${
-  words.map((x) => `1. [${x.title}](${x.url})`)
+words.map((x) => `1. [${x.title}](${x.url})`)
       .join("\n")
  }
 <!-- END -->`;
