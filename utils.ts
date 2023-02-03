3eaf1b63a9e8eb8d4cd763e48cb3f08de56ff754
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-02-03 22:25:34 +0900

    fix util

--- utils.ts
+++ utils.ts
@@ -23,10 +23,7 @@ export async function createReadme(words: Question[]): Promise<string> {
 export function createList(words: Question[]): string {
   return `<!-- BEGIN -->
 <!-- UpdateTime ${Date()} -->
-${
-words.map((x) => `1. [${x.title}](${x.url})`)
-      .join("\n")
- }
+${words.map((x) => `1. [${x.title}](${x.url})`).join("\n")}
 <!-- END -->`;
 }
 
